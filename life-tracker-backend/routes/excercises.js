dcdde09c1a56b671e32afcd0229dc6ced17030bb
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-06-28 11:43:19 -0500

    initial excercise functionality and routes

---
+++ life-tracker-backend/routes/excercises.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const Excercise = require("../models/excercise")
+const security = require("../middleware/security")
+const router = express.Router()
+
+router.get("/", security.requireAuthenticatedUser, async (req, res, next) => {
+    try {
+      const user = res.locals.user
+      const orders = await Excercise.listExcercisesForUser(user)
+      return res.status(200).json({ orders })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+
+router.post("/", security.requireAuthenticatedUser, async (req, res, next) => {
+    try {
+      const user = res.locals.user
+      const excercise = await Excercise.createExcercise({ user, excercise: req.body })
+      return res.status(201).json({ excercise }) //excercise: excercise object
+    } catch (err) {
+      next(err)
+    }
+  })
+
+
+  module.exports = router
