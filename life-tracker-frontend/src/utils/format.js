4e4b9a8101759f69d27334e94a1f3b48641bb074
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-06-29 10:37:47 -0500

    added timestamp date formatting

--- life-tracker-frontend/src/utils/format.js
+++ life-tracker-frontend/src/utils/format.js
@@ -15,3 +15,28 @@ export const formatPrice = (amount) => {
   const dollars = amount * 0.01
   return `$${formatter.format(dollars)}`
 }
+
+export const formatDateLabel = (date) => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    date = new Date(date.replace(' ', 'T'));
+    var a = date
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var hours = ((hour + 11) % 12) + 1;
+    var amPm = hour > 11 ? 'pm' : 'am';
+    var time = date + ' ' + month + ' ' + year + ' ' + hours + ':' + min + amPm;
+    if(year === yyyy && dd === date && mm === month) { time = "Today at " + hours + ':' + min + amPm;}
+    if(year === yyyy && dd-1 === date && mm === month) { time = "Yesterday at " + hours + ':' + min + amPm;}
+    
+    return time;
+
+}
