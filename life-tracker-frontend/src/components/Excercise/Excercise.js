273072541bfb5975604de39fea68ae58341c7699
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-06-29 12:01:49 -0500

    removing unused logic/lib

--- life-tracker-frontend/src/components/Excercise/Excercise.js
+++ life-tracker-frontend/src/components/Excercise/Excercise.js
@@ -6,10 +6,6 @@ import Box from '@material-ui/core/Box';
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Container from '@material-ui/core/Container';
-import { formatDateLabel } from "../../utils/format";
 import CardList from '../CardList/CardList';
 
 const useStyles = makeStyles({
@@ -88,47 +84,3 @@ export default function Excercise({ user, excercise }) {
         </div>
     )
 }
-
-{/* <Card className={classNames(classes.custom)} variant="outlined">
-             <CardContent className={classes.cardContentContainer}>
-            <Typography align="center" className={classNames(classes.title)} >
-             {excercise.excercises[0].name}
-             </Typography>
-             <Box
-        display="flex"
-        flexWrap="wrap"
-        p={1}
-        // bgcolor="background.paper"
-        css={{ maxWidth: 300 }}
-        justifyContent = 'space-between'
-        borderRadius={10}
-      >
-        <Box p={1} bgcolor="#2CB164" borderRadius={10}>
-            <Typography align="center" className={classNames(classes.subTitle)} >
-             Duration
-             </Typography>
-             <Typography align="center" className={classNames(classes.subTitle)} >
-             {excercise.excercises[0].duration}
-             </Typography>
-        </Box>
-        <Box p={1} bgcolor="#2CB164" borderRadius={10} mb={4}>
-            <Typography align="center" className={classNames(classes.subTitle)} >
-            Intensity
-             </Typography>
-             <Typography align="center" className={classNames(classes.subTitle)} >
-             {excercise.excercises[0].intensity}
-             </Typography>
-        </Box>
-        <Box justifyContent="center" alignItems="center" p={1} bgcolor="#2CB164" borderRadius={10}>
-            <Typography align="center" className={classNames(classes.timeStamp)} >
-            {formatDateLabel(excercise.excercises[0].postedAt)}
-             </Typography>
-        </Box>
-        <Box justifyContent="center" alignItems="center" border={1} p={.5} px={1} bgcolor="#2CB164" borderRadius={10}>
-            <Typography align="center" className={classNames(classes.subTitle)} >
-            {excercise.excercises[0].category}
-             </Typography>
-        </Box>
-        </Box>  
-        </CardContent>
-    </Card> */}
