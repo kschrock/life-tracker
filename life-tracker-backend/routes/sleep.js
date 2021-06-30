034d69e76cb5b53af394e70940963df7a2de41bb
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-06-30 13:43:57 -0500

    added new sleep backend logic and routes

---
+++ life-tracker-backend/routes/sleep.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const Sleep = require("../models/sleep")
+const security = require("../middleware/security")
+const router = express.Router()
+
+router.get("/", security.requireAuthenticatedUser, async (req, res, next) => {
+    try {
+      const user = res.locals.user
+      const sleep = await Sleep.listSleepForUser(user)
+      return res.status(200).json({ sleep })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+
+router.post("/", security.requireAuthenticatedUser, async (req, res, next) => {
+    try {
+      const user = res.locals.user
+      const sleep = await Sleep.createSleep({ user, sleep: req.body })
+      return res.status(201).json({ sleep }) //excercise: excercise object
+    } catch (err) {
+      next(err)
+    }
+  })
+
+
+  module.exports = router
