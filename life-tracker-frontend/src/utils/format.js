8e74d7ae208f55f866ff49627dc23cd4a39afb99
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-07-01 15:51:54 -0500

    added new formatting functions for datetime formats

--- life-tracker-frontend/src/utils/format.js
+++ life-tracker-frontend/src/utils/format.js
@@ -45,3 +45,62 @@ export const formatDateLabel = (date) => {
     return time;
 
 }
+
+export const formatDateLabelNoTime = (date) => {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2);
+  var mm = String(today.getMonth() + 1).padStart(2); //January is 0!
+  dd = Number(dd)
+  mm = Number(mm)
+  mm = mm -1
+  var yyyy = today.getFullYear();
+
+  date = new Date(date.replace(' ', 'T'));
+  var a = date
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var currentMonth = months[mm]
+  date = a.getDate();
+  var min = a.getMinutes();
+  var time = month + ' ' + date + ', ' + year ;
+  if(min<10){min = "0" + min} //adding 00-09
+  
+  if(year === yyyy && dd === date && currentMonth === month) { time = "Today"}
+  if(year === yyyy && dd-1 === date && currentMonth === month) { time = "Yesterday"}
+  
+  return time;
+
+}
+
+export const formatDateLabelOnlyTime = (date) => {
+  date = new Date(date.replace(' ', 'T'));
+  var a = date
+ 
+  date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var hours = ((hour + 11) % 12) + 1;
+  var amPm = hour > 11 ? 'pm' : 'am';
+  if(min<10){min = "0" + min} //adding 00-09
+  var time = hours + ':' + min + amPm;
+  
+  
+  return time;
+
+}
+
+const round = (number, decimalPlaces) => {
+  const factorOfTen = Math.pow(10, decimalPlaces)
+  return Math.round(number * factorOfTen) / factorOfTen
+}
+
+export const getTimeFromDates = (start, end) => {
+  let date1 = new Date(start.replace(' ', 'T'));
+  let date2 = new Date(end.replace(' ', 'T'));
+  let hours = Math.abs(date1 - date2) / 36e5;
+  hours= round(hours,2)
+
+  return (hours);
+
+}
