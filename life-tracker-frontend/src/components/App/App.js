6c933ea170df6cb61b7e8975d7823eebeee5633f
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-07-01 15:50:00 -0500

    app js now retains data on refresh for Excercise, Nutrition, Sleep, and Token

--- life-tracker-frontend/src/components/App/App.js
+++ life-tracker-frontend/src/components/App/App.js
@@ -10,6 +10,8 @@ import Excercise from "../Excercise/Excercise";
 import CreateExcercise from "../CreateExcercise/CreateExcercise";
 import Nutrition from "../Nutrition/Nutrition";
 import CreateNutrition from "../CreateNutrition/CreateNutrition"
+import Sleep from "../Sleep/Sleep";
+import CreateSleep from "../CreateSleep/CreateSleep";
 
 export const URL = process.env.REACT_APP_REMOTE_HOST_URL || "http://localhost:3001"
 
@@ -19,6 +21,38 @@ function App() {
   const [error, setError] = useState(null)
   const [excercise, setExcercise] = useState([])
   const [nutrition, setNutrition] = useState([])
+  const [sleep, setSleep] = useState([])
+
+  const fetchExercises = async () => {
+    setIsFetching(true)
+    if(user){
+    const {data, error } = await apiClient.getExcercises()
+    if(data) setExcercise(data.excercises)
+    //console.log(data.excercises)
+    if(error) setError(error)
+    setIsFetching(false)
+  }
+  }
+  const fetchSleep = async () => {
+    setIsFetching(true)
+    if(user){
+    const {data, error } = await apiClient.getSleep()
+    if(data) setSleep(data.sleep)
+    //console.log(data.excercises)
+    if(error) setError(error)
+    setIsFetching(false)
+  }
+  }
+  const fetchNutrition = async () => {
+    setIsFetching(true)
+    if(user){
+    const {data, error } = await apiClient.getNutrition()
+    if(data) setNutrition(data.nutrition)
+    //console.log(data.excercises)
+    if(error) setError(error)
+    setIsFetching(false)
+  }
+}
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -42,46 +76,28 @@ function App() {
   }, [])
 
   useEffect(() => {
-    const fetchExercises = async () => {
-      setIsFetching(true)
-      if(user){
-      const {data, error } = await apiClient.getExcercises()
-      if(data) setExcercise(data.excercises)
-      //console.log(data.excercises)
-      if(error) setError(error)
-      setIsFetching(false)
+    if(Object.keys(user).length === 0){
+      //No user data, so do nothing
     }
+    else if(Object.keys(user).length !== 0 && user){
+      //check if has data then make data calls
+      //console.log(user)
+      fetchExercises()
+      fetchNutrition()
+      fetchSleep()
     }
-    const fetchNutrition = async () => {
-      setIsFetching(true)
-      if(user){
-      const {data, error } = await apiClient.getNutrition()
-      if(data) setNutrition(data.nutrition)
-      //console.log(data.excercises)
-      if(error) setError(error)
-      setIsFetching(false)
-    }
-    }
-    fetchExercises()
-    fetchNutrition()
-  }, [])
+    }, [user])
 
-  const fetchExercises = async () => {
-    setIsFetching(true)
-    if(user){
-    const {data, error } = await apiClient.getExcercises()
-    if(data) setExcercise(data.excercises)
-    //console.log(data.excercises)
-    if(error) setError(error)
-    setIsFetching(false)
-  }
-  }
+
+ 
+
 
   const handleLogout = async () => {
     console.log("Logged OUT")
     await apiClient.logoutUser()
     setUser({}) //empty user
     setExcercise([]) //empty excercise list
+    setNutrition([])//empty nutrition list
     setError(null)
     //console.log(user)
   }
@@ -99,11 +115,13 @@ function App() {
         isFetching={isFetching}
         />} />
         <Route path="/register" element={<Register user={user} setUser={setUser}/>} />
-        <Route path="/login" element={<Login user={user} setUser={setUser} fetchExercises={fetchExercises}/>} />
+        <Route path="/login" element={<Login user={user} setUser={setUser} fetchExercises={fetchExercises} fetchSleep={fetchSleep} fetchNutrition={fetchNutrition}/>} />
         <Route path="/excercise" element={<Excercise user={user} excercise={excercise}/>} />
         <Route path="/excercise/create" element={<CreateExcercise  setExcercise={setExcercise} />} />
         <Route path="/nutrition" element={<Nutrition user={user} nutrition={nutrition}/>} />
         <Route path="/nutrition/create" element={<CreateNutrition  setNutrition={setNutrition} />} />
+        <Route path="/sleep" element={<Sleep user={user} sleep={sleep} />} />
+        <Route path="/sleep/create" element={<CreateSleep  setSleep={setSleep} />} />
      </Routes>
      </BrowserRouter>
     </div>
